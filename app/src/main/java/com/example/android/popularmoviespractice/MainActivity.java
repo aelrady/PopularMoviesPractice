04901d94061ddc6954850cdad67adb1c06790ff8
aelrady <ashley.elrady@gmail.com>
2018-05-13 00:30:10 -0400

    Add onSaveInstanceState(), check for internet connection before calling API, and display "No Internet Connection" text if there is no internet connection

--- app/src/main/java/com/example/android/popularmoviespractice/MainActivity.java
+++ app/src/main/java/com/example/android/popularmoviespractice/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.android.popularmoviespractice;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -7,6 +10,8 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.popularmoviespractice.api.MovieApiClient;
@@ -21,11 +26,13 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185";
-    private String sort_param;
+    private String sort_param = "popular";
+    GridLayoutManager gridLayoutManager;
     private RecyclerView recyclerView;
     private ArrayList<String> picturePathList;
     private ArrayList<Movie> movies;
     private MovieAdapter movieAdapter;
+    private TextView connectionTextView;
 
 
     @Override
@@ -33,18 +40,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState == null || !savedInstanceState.containsKey("movies")) {
-            populateGrid();
+            if (savedInstanceState == null || !savedInstanceState.containsKey("movies")) {
+                if (isConnected()) {
+                    populateGrid();
+                    connectionTextView = findViewById(R.id.no_connection);
+                    connectionTextView.setVisibility(View.GONE);
+                } else {
+                    connectionTextView = findViewById(R.id.no_connection);
+                    connectionTextView.setVisibility(View.VISIBLE);
+                }
+            } else {
+                movies = savedInstanceState.getParcelableArrayList("movies");
+                Log.v("Movies: ", String.valueOf(movies));
+                picturePathList = savedInstanceState.getStringArrayList("pictures");
+                Log.v("Picture paths: ", String.valueOf(picturePathList));
+                recyclerView = findViewById(R.id.rv_movies);
+                gridLayoutManager = new GridLayoutManager(this, 2);
+                recyclerView.setLayoutManager(gridLayoutManager);
+                movieAdapter = new MovieAdapter(MainActivity.this, picturePathList, movies);
+                recyclerView.setAdapter(movieAdapter);
+            }
         }
-        else {
-            movies = savedInstanceState.getParcelableArrayList("movies");
-            Log.v("Movies: ", String.valueOf(movies));
-            picturePathList = savedInstanceState.getStringArrayList("pictures");
-            Log.v("Picture paths: ", String.valueOf(picturePathList));
-            movieAdapter = new MovieAdapter(MainActivity.this, picturePathList, movies);
-            recyclerView.setAdapter(movieAdapter);
-        }
-    }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
@@ -61,14 +77,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.most_popular) {
-            sort_param = "popular";
-            populateGrid();
-        } else if(item.getItemId() == R.id.highest_rated) {
-            sort_param = "top_rated";
-            populateGrid();
+        if (isConnected()) {
+            connectionTextView = findViewById(R.id.no_connection);
+            connectionTextView.setVisibility(View.GONE);
+            if (item.getItemId() == R.id.most_popular) {
+                sort_param = "popular";
+                populateGrid();
+            } else if (item.getItemId() == R.id.highest_rated) {
+                sort_param = "top_rated";
+                populateGrid();
+            } else {
+                return super.onOptionsItemSelected(item);
+            }
         } else {
-            return super.onOptionsItemSelected(item);
+            connectionTextView = findViewById(R.id.no_connection);
+            connectionTextView.setVisibility(View.VISIBLE);
         }
         return true;
     }
@@ -76,32 +99,41 @@ public class MainActivity extends AppCompatActivity {
     public void populateGrid() {
 
         final String your_api_key = "f3a1a00157a6bc8750d0d6a07bfd9811";
-        sort_param = "popular";
 
         recyclerView = findViewById(R.id.rv_movies);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        gridLayoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(gridLayoutManager);
 
         MovieApiInterface movieApiInterface = MovieApiClient.getClient().create(MovieApiInterface.class);
 
-        Call<MovieResults> call = movieApiInterface.getMovies(sort_param, your_api_key);
-        call.enqueue(new Callback<MovieResults>() {
-            @Override
-            public void onResponse(Call<MovieResults> call, Response<MovieResults> response) {
-                movies = (ArrayList<Movie>) response.body().getResults();
-                picturePathList = new ArrayList<>();
-                for (int i = 0; i < movies.size(); i++) {
-                    picturePathList.add(IMAGE_BASE_URL + movies.get(i).getPosterPath());
+            Call<MovieResults> call = movieApiInterface.getMovies(sort_param, your_api_key);
+            call.enqueue(new Callback<MovieResults>() {
+                @Override
+                public void onResponse(Call<MovieResults> call, Response<MovieResults> response) {
+                    movies = (ArrayList<Movie>) response.body().getResults();
+                    picturePathList = new ArrayList<>();
+                    for (int i = 0; i < movies.size(); i++) {
+                        picturePathList.add(IMAGE_BASE_URL + movies.get(i).getPosterPath());
+                    }
+                    movieAdapter = new MovieAdapter(MainActivity.this, picturePathList, movies);
+                    recyclerView.setAdapter(movieAdapter);
                 }
-                movieAdapter = new MovieAdapter(MainActivity.this, picturePathList, movies);
-                recyclerView.setAdapter(movieAdapter);
-            }
 
-            @Override
-            public void onFailure(Call<MovieResults> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "failure", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+                @Override
+                public void onFailure(Call<MovieResults> call, Throwable t) {
+                    Toast.makeText(MainActivity.this, "failure", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    private boolean isConnected() {
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return isConnected;
+    };
 }
 
